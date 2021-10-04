a2595f15d05298504f69aeb28fb418a5bcc745ff
Nick Palladino <np398@cornell.edu>
2021-10-04 16:40:20 -0400

    Added observations plot

--- brapi_phenotype_qc.py
+++ brapi_phenotype_qc.py
@@ -6,14 +6,13 @@
 
 import dash
 import dash_html_components as html
-import plotly.graph_objects as go
-import dash_core_components as dcc
 import plotly.express as px
+import dash_core_components as dcc
 from dash.dependencies import Input, Output, State
+import pandas as pd
 import brapi_v2
 
 app = dash.Dash()
-brapi_service = brapi_v2.BrAPIPhenotypeService()
     
 def get_study_options(studies_dict):
     return list(map(lambda study: {'label': study['studyName'], 'value': study['studyDbId']}, studies_dict))
@@ -22,18 +21,36 @@ app.layout = html.Div(id = 'parent', children = [
     html.H1(id='H1', children='BrAPI Phenotype QC', style={'textAlign':'center'}),
         dcc.Input(id='brapi-url', placeholder='Enter a url...', type='text', value=''),
         html.Button(id='get-studies-button', n_clicks=0, children='Get Studies'),
-        dcc.Dropdown(id='studies-dropdown', options=get_study_options(brapi_service.get_studies_dict())),
-        html.Div(id='output-state'),
+        dcc.Dropdown(id='studies-dropdown'),
+        dcc.Graph(id='observations_plot'),
+        dcc.Store(id='brapi-url-state')
     ])
 
-@app.callback(Output('studies-dropdown', 'options'),
+@app.callback([Output('studies-dropdown', 'options'), Output('brapi-url-state', 'data')],
               Input('get-studies-button', 'n_clicks'),
               State('brapi-url', 'value'))
 def update_studies(n_clicks, brapi_url):
     if brapi_url:
-        brapi_service.set_brapi_base_url(brapi_url)
-        brapi_service.get_brapi_studies()
-    return (get_study_options(brapi_service.get_studies_dict()))
+        brapi_service = brapi_v2.BrAPIPhenotypeService(brapi_url)
+        studies_list_dict = brapi_service.get_studies().get_data_list_dict()
+        return (get_study_options(studies_list_dict), brapi_url)
+    return ([], brapi_url)
+
+@app.callback(Output(component_id='observations_plot', component_property= 'figure'),
+              Input(component_id='studies-dropdown', component_property= 'value'),
+              Input('brapi-url-state', 'data'))
+def update_observations_plot(selected_study, brapi_url):
+    if selected_study:
+        brapi_service = brapi_v2.BrAPIPhenotypeService(brapi_url)
+        obs_df = brapi_service.get_observations_by_study_id(selected_study).get_data_df()
+        obs_df["value"] = pd.to_numeric(obs_df["value"], errors='coerce')
+        fig = px.scatter(obs_df, x='observationTimeStamp', y='value')
+        fig.update_layout(title = 'Observation values over time',
+                          xaxis_title = 'Date',
+                          yaxis_title = 'Value'
+                          )
+        return fig 
+    return px.scatter()
 
 if __name__ == '__main__': 
     app.run_server(debug=True, dev_tools_silence_routes_logging = False)
--- brapi_v2.py
+++ brapi_v2.py
@@ -10,9 +10,25 @@ import pandas as pd
 def dict_to_dataframe(brapi_dict):
     return pd.DataFrame(brapi_dict)
 
+class BrAPIQueryParams:
+    
+    def __init__(self):
+        self.params = {'pageSize':'10000'}
+
+class BrAPIDataResponse:
+    
+    def __init__(self, data = []):
+        self.data = data
+        
+    def get_data_list_dict(self):
+        return self.data
+    
+    def get_data_df(self):
+        return dict_to_dataframe(self.data)
+
 class BrAPIClient:
     
-    DEFAULT_QUERY_PARAMS = {'pageSize':'10000'}
+    DEFAULT_QUERY_PARAMS = BrAPIQueryParams().params
     DEFAULT_TIMEOUT = 60
 
     def __init__(self, base_url=None):
@@ -22,26 +38,28 @@ class BrAPIClient:
         self.base_url = base_url
         
     def get(self, endpoint, params=DEFAULT_QUERY_PARAMS, timeout=DEFAULT_TIMEOUT):
+        print(params)
         response = requests.get(self.base_url+"/brapi/v2/"+endpoint, params=params, timeout=timeout)
         return response.json()['result']['data']
     
 class BrAPIPhenotypeService:
     
-    def __init__(self):
-        self.client = BrAPIClient()
-        self.studies = []
+    def __init__(self, base_url):
+        self.client = BrAPIClient(base_url)
     
     def set_brapi_base_url(self, brapi_base_url):
         self.client.set_base_url(brapi_base_url)
         
-    def get_brapi_studies(self):
-        self.studies = self.client.get('studies')
-        
-    def get_studies_dict(self):
-        return self.studies
+    def get_studies(self):
+        return BrAPIDataResponse(self.client.get('studies'))
     
-    def get_studies_df(self):
-        return dict_to_dataframe(self.studies)
-
+    def get_observations(self):
+        return BrAPIDataResponse(self.client.get('observations'))
+        
+    def get_observations_by_study_id(self, study_id):
+        query = BrAPIQueryParams()
+        query.params['studyDbId'] = study_id
+        return BrAPIDataResponse(self.client.get('observations', query.params))
+        
 
 
