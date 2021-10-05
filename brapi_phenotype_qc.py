527f577ee26744724e9ef3f7d6074ec0d757ee51
Nick Palladino <np398@cornell.edu>
2021-10-05 08:40:56 -0400

    Added plot point selection and table display

--- brapi_phenotype_qc.py
+++ brapi_phenotype_qc.py
@@ -9,6 +9,7 @@ import dash_html_components as html
 import plotly.express as px
 import dash_core_components as dcc
 from dash.dependencies import Input, Output, State
+import dash_table as dt
 import pandas as pd
 import brapi_v2
 
@@ -22,8 +23,10 @@ app.layout = html.Div(id = 'parent', children = [
         dcc.Input(id='brapi-url', placeholder='Enter a url...', type='text', value=''),
         html.Button(id='get-studies-button', n_clicks=0, children='Get Studies'),
         dcc.Dropdown(id='studies-dropdown'),
-        dcc.Graph(id='observations_plot'),
-        dcc.Store(id='brapi-url-state')
+        dcc.Graph(id='observations-plot'),
+        dcc.Store(id='brapi-url-state'),
+        dcc.Store(id='brapi-observations-data'),
+        dt.DataTable(id='observations-table')
     ])
 
 @app.callback([Output('studies-dropdown', 'options'), Output('brapi-url-state', 'data')],
@@ -36,21 +39,36 @@ def update_studies(n_clicks, brapi_url):
         return (get_study_options(studies_list_dict), brapi_url)
     return ([], brapi_url)
 
-@app.callback(Output(component_id='observations_plot', component_property= 'figure'),
+@app.callback([Output(component_id='observations-plot', component_property= 'figure'),
+               Output('brapi-observations-data', 'data')],
               Input(component_id='studies-dropdown', component_property= 'value'),
               Input('brapi-url-state', 'data'))
 def update_observations_plot(selected_study, brapi_url):
     if selected_study:
         brapi_service = brapi_v2.BrAPIPhenotypeService(brapi_url)
-        obs_df = brapi_service.get_observations_by_study_id(selected_study).get_data_df()
-        obs_df["value"] = pd.to_numeric(obs_df["value"], errors='coerce')
-        fig = px.scatter(obs_df, x='observationTimeStamp', y='value')
+        obs_data = brapi_service.get_observations_by_study_id(selected_study)
+        obs_df = obs_data.get_data_df()
+        obs_df['value'] = pd.to_numeric(obs_df['value'], errors='coerce')
+        fig = px.scatter(obs_df, x='observationTimeStamp', y='value', custom_data=['observationDbId'])
         fig.update_layout(title = 'Observation values over time',
                           xaxis_title = 'Date',
                           yaxis_title = 'Value'
                           )
-        return fig 
-    return px.scatter()
+        return fig, obs_data.get_data_list_dict()
+    return px.scatter(), []
+
+@app.callback([Output('observations-table', 'columns'),
+               Output('observations-table', 'data')],
+              Input('observations-plot', 'selectedData'),
+              Input('brapi-observations-data', 'data'))
+def display_selected_data(selectedData, observations):
+    if selectedData and len(selectedData['points']) > 0:
+        selected_obs_ids = [data['customdata'][0] for data in selectedData['points']]
+        filtered_obs = [obs for obs in observations if obs['observationDbId'] in selected_obs_ids]
+        clean_obs = [{k:v for k,v in obs.items() if v} for obs in filtered_obs]
+        cols=[{'name': i, 'id': i} for i in clean_obs[0].keys()]
+        return cols, clean_obs
+    return [], []
 
 if __name__ == '__main__': 
     app.run_server(debug=True, dev_tools_silence_routes_logging = False)
